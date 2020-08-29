6bf9981cd3343d97312c2241579466de87d623a2
yihanYozikua <newy860318@gmail.com>
2020-08-29 23:52:24 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from os.path import basename
+import sys
+import json
+import requests
+import cv2
+import numpy as np
+import logging
+from io import BytesIO
+
+# take imgs
+def shot_cv2():
+  
+
+# OCR
+def ocr():
+
+# text sort out
+def sort_text():
+
+
+# database
+def connect_to_db():
+
+
+if __name__ == '__main__':
+
+  try:
+    shot_cv2()
+    ocr()
+  except:
+    logging.exception("Message")
